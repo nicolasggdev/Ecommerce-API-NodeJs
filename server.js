ae1eeb8b065721a2623f7e8f0771f9c6944a5f16
nicolasggdev <nicolasggdev@gmail.com>
2022-03-30 05:22:36 -0500

    feat: successful database connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // Import App
 const { app } = require("./app");
 
+// Import Database
+const { database } = require("./database/database");
+
 // Import dotenv
 const dotenv = require("dotenv");
 
 // Init dotenv
 dotenv.config({ path: "./config.env" });
+
+// Database Authenticated
+database
+  .authenticate()
+  .then(() => console.log("Database is authenticated"))
+  .catch((err) => console.log(err));
 
 // Create a server on http://localhost:4000
 const PORT = process.env.PORT || 4000;
